67791a60b6cdf8aefd79b16e7edc2e0e86f8923d
ZKkkk <dxfdyxddd@126.com>
2024-12-08 18:58:33 +0800

    fix: project name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/gh-action-react/", // 替换为你的项目名
   plugins: [react()],
-})
+});
