0188a6187af2b620920934e29f831ef532e1f55e
ZKkkk <dxfdyxddd@126.com>
2024-12-08 19:40:59 +0800

    feat: upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // https://codeusb.github.io/gh-action-react/
   base: "/gh-action-react/", // 替换为你的项目名
   plugins: [react()],
 });
